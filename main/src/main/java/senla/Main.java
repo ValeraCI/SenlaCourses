1181574e140f9a5cc795505137ec4e430c44516a
unknown <cidikvakera@gmail.com>
2023-01-17 18:58:32 +0300

    Added logging using log4j and slf4j technologies

--- main/src/main/java/senla/Main.java
+++ main/src/main/java/senla/Main.java
@@ -1,12 +1,22 @@
 package senla;
 
 import framework.source.ApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import senla.controllers.MusicController;
 
+import java.time.LocalDateTime;
+
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        logger.info("Application has started, time= {}", LocalDateTime.now());
+
         ApplicationContext applicationContext = ApplicationContext.createContext("senla");
         MusicController musicController = applicationContext.getBean(MusicController.class);
         System.out.println(musicController.execute());
+
+        logger.info("Application has ended, time= {}", LocalDateTime.now());
     }
 }
--- main/src/main/java/senla/controllers/MusicController.java
+++ main/src/main/java/senla/controllers/MusicController.java
@@ -2,10 +2,13 @@ package senla.controllers;
 
 import framework.annotations.Autowired;
 import framework.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import senla.services.Service;
 
 @Component
 public class MusicController {
+    private static final Logger logger = LoggerFactory.getLogger(MusicController.class);
     private final Service musicService;
 
     @Autowired
@@ -14,6 +17,9 @@ public class MusicController {
     }
 
     public String execute() {
-        return musicService.execute();
+        logger.debug("Method execute started");
+        String text = musicService.execute();
+        logger.debug("Method execute return '{}'", text);
+        return text;
     }
 }
